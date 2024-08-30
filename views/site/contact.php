86ccdf3f3e2250af778ea86f5f69f992e4423cb1
Samwel <samwelnyandoro01@gmail.com>
2024-08-30 10:36:59 +0300

    modified views/site/contact.php

--- views/site/contact.php
+++ views/site/contact.php
@@ -40,15 +40,10 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="col-lg-5">
 
                 <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-
                     <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
-
                     <?= $form->field($model, 'email') ?>
-
                     <?= $form->field($model, 'subject') ?>
-
                     <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
-
                     <?= $form->field($model, 'verifyCode')->widget(Captcha::class, [
                         'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
                     ]) ?>
