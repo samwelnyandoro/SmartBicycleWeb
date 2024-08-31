6280aae88d2dec8195c508adc0e0aca0fab55089
Samwel <samwelnyandoro01@gmail.com>
2024-09-01 00:04:22 +0300

    modified views/site/index.php

--- views/site/index.php
+++ views/site/index.php
@@ -8,7 +8,6 @@ $this->title = 'My Yii Application';
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
         <h1 class="display-4">Congratulations!</h1>
-
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
         <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
