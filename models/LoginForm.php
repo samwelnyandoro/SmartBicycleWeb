4efdc6fe06ae3a84a945abc2787254ee72c86b9f
Samwel <samwelnyandoro01@gmail.com>
2024-09-04 09:17:30 +0300

    modified models/LoginForm.php

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -43,7 +43,6 @@ class LoginForm extends Model
     {
         if (!$this->hasErrors()) {
             $user = $this->getUser();
-
             if (!$user || !$user->validatePassword($this->password)) {
                 $this->addError($attribute, 'Incorrect username or password.');
             }
